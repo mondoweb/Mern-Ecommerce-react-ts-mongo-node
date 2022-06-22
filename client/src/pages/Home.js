4a71a71182b3b56f43a43463ab2020c3686ca79b
lucadibaa <lucadibaa@protonmail.com>
2022-06-22 11:29:52 +0200

    initial commit

---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import Banner from "../components/home/Banner"
+
+const Home = () => {
+    return (
+        <div>
+            <Banner />
+            Prodotti Frequenti
+            Recensioni
+        </div>
+    )
+}
+
+export default Home
